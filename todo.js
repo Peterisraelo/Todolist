2f1a75154d681353f3bea9f0eddf40bacf5f4414
Peter Israel <46018804+Peterisraelo@users.noreply.github.com>
2019-01-30 11:34:16 +0100

    Add files via upload

--- todo.js
+++ todo.js
@@ -9,14 +9,27 @@ function addnewtask(){
     var ul = document.getElementById("MyList");
     var li = document.createElement("li");
 
+    //Creating div to house each list item
+    var home= document.createElement("div");
+
     //Creating close button
     var close = document.createElement("p");
-    close.appendChild(document.createTextNode("x"));
+    close.appendChild(document.createTextNode("X"));
+    close.className="closer";
 
+
+    
     li.appendChild(document.createTextNode(txt));
-    ul.appendChild(li);
-    ul.appendChild(close);
+    ul.appendChild(home);
+    home.appendChild(li);
+    home.appendChild(close);
 
+    document.getElementById("additem").value="";
+
+    close.onclick= function(){
+        li.remove();
+        close.remove();
+    }
     txt.value = "";
     (li).onclick = function(){
         li.style.textDecoration= "line-through";
@@ -26,5 +39,6 @@ function addnewtask(){
 document.body.onkeyup = function(e) {
     if (e.keyCode == 13) {
       addnewtask();
+      document.getElementById("additem").textContent.remove();
     }
   }
