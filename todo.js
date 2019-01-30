16c16b0e777c0ba9dea3af274e03c377c75b8536
Peterisraelo <46018804+Peterisraelo@users.noreply.github.com>
2019-01-30 10:17:21 +0100

    Add files via upload

--- todo.js
+++ todo.js
@@ -3,35 +3,28 @@ var d= new Date();
 document.getElementById("todaytop").innerHTML += " " + d.getDate() +""+((d.getMonth())-1) + "-"+d.getFullYear();
 
 //Adding new item to to do list
-var put= document.createElement("input");
-var li = document.createElement("li");
 
 function addnewtask(){
     var txt = document.getElementById("additem").value;
     var ul = document.getElementById("MyList");
     var li = document.createElement("li");
-    //checkbox changes
-    var put= document.createElement("input");
-    put.className="check"
-    put.type="checkbox";
-    // li.appendChild(put);
-    // put.onclick=FinishedTask();
-    //checkbox changes
+
+    //Creating close button
+    var close = document.createElement("p");
+    close.appendChild(document.createTextNode("x"));
+
     li.appendChild(document.createTextNode(txt));
     ul.appendChild(li);
-    li.appendChild(put);
-    put.onclick= FinishedTask();
-    document.getElementById("additem").value="";
-    li.onclick= FinishedTask();
+    ul.appendChild(close);
+
+    txt.value = "";
+    (li).onclick = function(){
+        li.style.textDecoration= "line-through";
+        li.style.color= "grey";
+    }
 }
 document.body.onkeyup = function(e) {
     if (e.keyCode == 13) {
       addnewtask();
     }
-  }
-
-  function FinishedTask(){
-      put.className="checked";
-      li.className="done";
-
   }
